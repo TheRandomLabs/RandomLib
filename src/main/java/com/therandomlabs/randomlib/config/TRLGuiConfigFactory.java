8285400c41fd83873d47b49cfc833172c405420f
TheRandomLabs <therandomlabsinc@gmail.com>
2019-03-09 13:42:10 +1100

    Fix

--- src/main/java/com/therandomlabs/randomlib/config/TRLGuiConfigFactory.java
+++ src/main/java/com/therandomlabs/randomlib/config/TRLGuiConfigFactory.java
@@ -1,10 +1,27 @@
 package com.therandomlabs.randomlib.config;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
+import com.therandomlabs.randomlib.TRLUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.client.IModGuiFactory;
+import net.minecraftforge.fml.client.config.GuiConfig;
 
 public abstract class TRLGuiConfigFactory implements IModGuiFactory {
+	@Override
+	public GuiScreen createConfigGui(GuiScreen parentScreen) {
+		try {
+			return mainConfigGuiClass().getDeclaredConstructor(GuiScreen.class).
+					newInstance(parentScreen);
+		} catch(NoSuchMethodException | InstantiationException | IllegalAccessException |
+				InvocationTargetException ex) {
+			TRLUtils.crashReport("Failed to create configuration GUI", ex);
+		}
+
+		return null;
+	}
+
 	@Override
 	public void initialize(Minecraft mc) {}
 
@@ -17,4 +34,6 @@ public abstract class TRLGuiConfigFactory implements IModGuiFactory {
 	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
 		return null;
 	}
+
+	public abstract Class<? extends GuiConfig> mainConfigGuiClass();
 }
--- src/test/java/com/therandomlabs/randomlib/test/config/RLTGuiConfigFactory.java
+++ src/test/java/com/therandomlabs/randomlib/test/config/RLTGuiConfigFactory.java
@@ -1,11 +1,11 @@
 package com.therandomlabs.randomlib.test.config;
 
 import com.therandomlabs.randomlib.config.TRLGuiConfigFactory;
-import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.config.GuiConfig;
 
 public class RLTGuiConfigFactory extends TRLGuiConfigFactory {
 	@Override
-	public GuiScreen createConfigGui(GuiScreen parentScreen) {
-		return new RLTGuiConfig(parentScreen);
+	public Class<? extends GuiConfig> mainConfigGuiClass() {
+		return RLTGuiConfig.class;
 	}
 }
