f93ea1d43ef87dc10a1ee766612554ba53d24b85
TheRandomLabs <therandomlabsinc@gmail.com>
2019-07-09 12:27:10 +1000

    EntityUtils

---
+++ src/main/java/com/therandomlabs/randomlib/EntityUtils.java
@@ -0,0 +1,14 @@
+package com.therandomlabs.randomlib;
+
+import net.minecraft.inventory.EntityEquipmentSlot;
+
+public final class EntityUtils {
+	public static final EntityEquipmentSlot[] ARMOR_SLOTS = {
+			EntityEquipmentSlot.HEAD,
+			EntityEquipmentSlot.CHEST,
+			EntityEquipmentSlot.LEGS,
+			EntityEquipmentSlot.FEET
+	};
+
+	private EntityUtils() {}
+}
