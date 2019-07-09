d240f03cb99062ed81385fba04ef8d893d89f12a
TheRandomLabs <therandomlabsinc@gmail.com>
2019-07-09 12:27:48 +1000

    EntityUtils

--- src/main/java/com/therandomlabs/randomlib/EntityUtils.java
+++ src/main/java/com/therandomlabs/randomlib/EntityUtils.java
@@ -1,14 +1,16 @@
 package com.therandomlabs.randomlib;
 
+import java.util.List;
+import com.google.common.collect.ImmutableList;
 import net.minecraft.inventory.EntityEquipmentSlot;
 
 public final class EntityUtils {
-	public static final EntityEquipmentSlot[] ARMOR_SLOTS = {
+	public static final List<EntityEquipmentSlot> ARMOR_SLOTS = ImmutableList.of(
 			EntityEquipmentSlot.HEAD,
 			EntityEquipmentSlot.CHEST,
 			EntityEquipmentSlot.LEGS,
 			EntityEquipmentSlot.FEET
-	};
+	);
 
 	private EntityUtils() {}
 }
