1f665a24886369c8bda5a52a69d819b60d99c8b5
TheRandomLabs <therandomlabsinc@gmail.com>
2019-10-26 20:28:28 +1100

    Double.MIN_VALUE -> -Double.MAX_VALUE

--- src/main/java/com/therandomlabs/randomlib/config/Config.java
+++ src/main/java/com/therandomlabs/randomlib/config/Config.java
@@ -34,7 +34,7 @@ public @interface Config {
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.FIELD)
 	@interface RangeDouble {
-		double min() default Double.MIN_VALUE;
+		double min() default -Double.MAX_VALUE;
 
 		double max() default Double.MAX_VALUE;
 	}
